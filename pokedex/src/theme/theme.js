3762996f76632deda770f40b09b33f11b51eeb79
Alan Brazões <alanbrazoes@gmail.com>
2021-11-18 22:34:09 -0300

    theme

---
+++ pokedex/src/theme/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@material-ui/core/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff4400',
+    },
+    secondary: {
+      light: '#0066ff',
+      main: '#0044ff',
+      contrastText: '#ffcc00',
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
